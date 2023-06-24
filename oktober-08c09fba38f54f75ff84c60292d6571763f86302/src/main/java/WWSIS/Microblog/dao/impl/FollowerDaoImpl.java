00c13b1d906eb9566c43e3e28aa2a348ef43fe51
marlenah3993 <marlenka3993@gmail.com>
2023-06-24 12:23:31 +0200

    Fixed some mistakes

---
+++ oktober-08c09fba38f54f75ff84c60292d6571763f86302/src/main/java/WWSIS/Microblog/dao/impl/FollowerDaoImpl.java
@@ -0,0 +1,17 @@
+package WWSIS.Microblog.dao.impl;
+
+import WWSIS.Microblog.dao.FollowerDao;
+
+public class FollowerDaoImpl implements FollowerDao {
+    public void addPair(int followerID, int followeeID) {
+
+    }
+
+    public void removePair(int followerID, int followeeID) {
+
+    }
+
+    public void checkPair(int followerID, int followeeID) {
+
+    }
+}
---
+++ oktober-08c09fba38f54f75ff84c60292d6571763f86302/src/main/java/WWSIS/Microblog/model/Follower.java
@@ -0,0 +1,60 @@
+package WWSIS.Microblog.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Table(name="follower")
+public class Follower {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int id;
+	// follower and following attributes should be
+	// String arrays but for now we are declaring
+	// them as just Strings
+	@Column(name="follower", nullable = true)
+	private String follower;
+	@Column(name="following", nullable = true)
+	private String following;
+	@Column(name="primary_key", nullable = true)
+	private int primaryKey;
+	
+	// @Column(name="unique", nullable = false);	
+	// private [type?] unique
+	
+	public Follower(int id, String follower, String following, int primaryKey /*, [type?] unique*/) {
+		this.id = id;
+		this.follower = follower;
+		this.following = following;
+		this.primaryKey = primaryKey;
+		// this.unique = unique;
+	}
+	
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	public int getId() {
+		return this.id;
+	}
+	
+	public void setFollower(String follower) {
+		this.follower = follower;
+	}
+	
+	public String getFollower() {
+		return this.follower;
+	}
+	
+	public void setFollowing(String following) {
+		this.following = following;
+	}
+	
+	public String getFollowing() {
+		return this.following;
+	}
+	
+}
