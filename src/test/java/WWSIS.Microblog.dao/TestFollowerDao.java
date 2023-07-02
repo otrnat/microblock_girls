dfdb234ef17f5ef943196b4f56c4e2c14605ec55
Natalia Otreshko <32938592+otrnat@users.noreply.github.com>
2023-07-02 18:38:40 +0300

    add POM in main directory and changing in the tests

--- src/test/java/WWSIS.Microblog.dao/TestFollowerDao.java
+++ src/test/java/WWSIS.Microblog.dao/TestFollowerDao.java
@@ -1,7 +1,7 @@
 package WWSIS.Microblog.dao;
 
-import staticorg.junit.Assert.assertFalse;
-import staticorg.junit.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
